a1f824794fe7ee1d21f1f70f519cec6cbf7b5082
TheBlackParrot <theblackparrot0@gmail.com>
2025-10-30 06:33:36 -0500

    split editor patches into their own class

---
+++ QRCodeUnlocker/Patches/EditorPatches.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace QRCodeUnlocker.Patches;
+
+[HarmonyPatch]
+internal static class EditorPatches
+{
+    private static Transform? _trackURLInputFieldTransform;
+    
+    [HarmonyPatch(typeof(ClipInfoEditorPanel), nameof(ClipInfoEditorPanel.HandleSaveButton))]
+    [HarmonyPrefix]
+    // ReSharper disable once InconsistentNaming
+    private static bool ClipInfoEditorPanel_HandleSaveButton_Patch(ClipInfoEditorPanel __instance)
+    {
+        string text = _trackURLInputFieldTransform?.GetComponent<XDInputField>().tmpField.text ?? string.Empty;
+        __instance.CurrentTrackInfo.spotifyLink = text;
+        
+        return true;
+    }
+    
+    [HarmonyPatch(typeof(ClipInfoEditorPanel), nameof(ClipInfoEditorPanel.OnEnable))]
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    private static void ClipInfoEditorPanel_OnEnable_Patch(ClipInfoEditorPanel __instance)
+    {
+        Transform? trackURLTransform = __instance.trackDetailsPanel.transform.Find("TrackURL");
+        if (trackURLTransform != null)
+        {
+            return;
+        }
+        
+        Transform? charterTransform = __instance.trackDetailsPanel.transform.Find("Charter");
+        if (charterTransform == null)
+        {
+            Plugin.Log.LogWarning("charterTransform is null");
+            return;
+        }
+
+        if (__instance.trackDetailsPanel.transform == null)
+        {
+            Plugin.Log.LogWarning("__instance.trackDetailsPanel.transform is null");
+            return;
+        }
+        
+        GameObject trackURLContainer = Object.Instantiate(charterTransform.gameObject, __instance.trackDetailsPanel.transform);
+        trackURLContainer.name = "TrackURL";
+        trackURLTransform = trackURLContainer.transform;
+        trackURLTransform.SetSiblingIndex(charterTransform.GetSiblingIndex() + 1);
+
+        Transform trackURLTitleArea = trackURLTransform.Find("CharterTitleArea");
+        trackURLTitleArea.name = "TrackURLTitleArea";
+        Transform trackURLInputArea = trackURLTransform.Find("CharterInputArea");
+        trackURLInputArea.name = "TrackURLInputArea";
+
+        Transform trackURLTitle = trackURLTitleArea.Find("CharterTitle");
+        trackURLTitle.name = "TrackURLTitle";
+        trackURLTitle.GetComponent<TranslatedTextMeshPro>().SetTranslationKey($"{nameof(QRCodeUnlocker)}_TrackURLLabel");
+
+        _trackURLInputFieldTransform = trackURLInputArea.Find("InputField (TMP)");
+        XDInputField inputField = _trackURLInputFieldTransform.GetComponent<XDInputField>();
+        
+        inputField.tmpField.SetText(string.Empty);
+        inputField.tmpField.OnStoppedEditing += () =>
+        {
+            if (inputField.tmpField.textWhenStartedEditing != inputField.tmpField.text)
+            {
+                __instance.CurrentTrackInfo.SetDirty();
+            }
+        };
+    }
+
+    [HarmonyPatch(typeof(TrackEditorGUI), nameof(TrackEditorGUI.OnEditClipInfoPressed))]
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    private static void TrackEditorGUI_OnEditClipInfoPressed_Patch(TrackEditorGUI __instance)
+    {
+        if (__instance.trackDataToEdit == null)
+        {
+            Plugin.Log.LogWarning("__instance.trackDataToEdit is null");
+            return;
+        }
+        if (_trackURLInputFieldTransform == null)
+        {
+            Plugin.Log.LogWarning("_trackURLInputFieldTransform is null");
+            return;
+        }
+
+        string link = __instance.trackDataToEdit.GetInfoRefForFirstSegment().asset.spotifyLink;
+        _trackURLInputFieldTransform.GetComponent<XDInputField>().tmpField.SetText(link);
+    }
+}
+
